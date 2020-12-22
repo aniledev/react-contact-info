727890a24b5bfa7255db0b5e84182d3153bb6a46
aniledev <elinamcgill@gmail.com>
2020-12-22 16:53:22 -0500

    chore: pass props from parent App to Table component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Table from "./Table";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const characters = [
+      {
+        name: "Charlie",
+        job: "Janitor",
+      },
+      {
+        name: "Orion",
+        job: "software Developer",
+      },
+      {
+        name: "Tyanna",
+        job: "Artist",
+      },
+      {
+        name: "Aisha",
+        job: "Manager",
+      },
+    ];
+
+    return (
+      <div className="container">
+        <Table characterData={characters} />
+      </div>
+    );
+  }
 }
 
 export default App;
