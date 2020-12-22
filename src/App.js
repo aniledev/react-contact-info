d3e26c7a79695fa6131829d8608de9195fcaf4e2
aniledev <elinamcgill@gmail.com>
2020-12-22 17:30:15 -0500

    chore: create removeCharacter function in App component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import Table from "./Table";
 import "./index.css";
 
+// props gives us a one way data flow, parent -> child
+// state allows us to update private data from a component
+
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: "Charlie",
         job: "Janitor",
       },
       {
         name: "Orion",
-        job: "software Developer",
+        job: "Software Developer",
       },
       {
         name: "Tyanna",
@@ -22,10 +25,27 @@ class App extends Component {
         name: "Aisha",
         job: "Manager",
       },
-    ];
+    ],
+  };
+
+  // this function removes a character from the state.characters array
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+    //this.setState is how you modify/update an array using a built in method for modifying state
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+  render() {
+    // create character data to pass through to Table component as a property/prop
+    const characters = [];
 
     return (
       <div className="container">
+        {/* Using the characters data to pass through to Table as a property*/}
         <Table characterData={characters} />
       </div>
     );
--- src/Table.js
+++ src/Table.js
@@ -11,16 +11,29 @@ const TableHeader = () => {
   );
 };
 
-const TableBody = () => {
-  return <tbody />;
+const TableBody = (props) => {
+  // once the data is accessed by the TableBody component it can be mapped over and used to create dynamic table rows
+  const rows = props.characterData.map((row, index) => {
+      return (
+        {/* Keys are used to give list items unqiue indentifiers*/}
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    );
+  });
+  return <tbody>{rows}</tbody>;
 };
 
 class Table extends Component {
   render() {
+    // characterData is a property of the Table component
+    const { characterData } = this.props;
     return (
       <table>
         <TableHeader />
-        <TableBody />
+        {/* property has to passed from App -> Table -> TableBody*/}
+        <TableBody characterData={characterData} />
       </table>
     );
   }
